1b862b9eb4fc229a59098fefa495b5dd23b6ebb6
tetepaez <tete.paez.007@gmail.com>
2021-07-03 17:53:46 -0300

    creating grid functions

---
+++ MatcheAdas.js
@@ -0,0 +1,53 @@
+//Llamando la grilla principal
+const mainGrid = document.querySelector('.grid');
+const boxes = [];
+let emojiRose = ['🐷', '🌷', '💗', '🤰🏾', '🧠', '👄', ];
+
+//Constante en donde guardo todas las características del tamaño de la matriz
+const levels = [{
+        key: 'easy',
+        size: 9,
+        label: 'facil',
+        fontSize: 30,
+        itemSize: 56
+    },
+    {
+        key: 'normal',
+        size: 8,
+        label: 'normal',
+        fontSize: 30,
+        itemSize: 62
+    },
+    {
+        key: 'difficult',
+        size: 7,
+        label: 'Dificil',
+        fontSize: 35,
+        itemSize: 72
+    }
+];
+let levelSize;
+
+//Variables para crear la función del elemento ó item clickeado e intercambiado por el de al lado
+let itemSelected = null;
+let clickedItem = null;
+
+
+//Creando la grilla principal
+const createGrid = (key) => {
+
+    //Constante que genera el array de dificultad
+    const level = levels.filter(l => l.key === key)[0];
+    levelSize = level.size;
+    mainGrid.innerHTML = '';
+
+    //Mediante ambos "for" estoy generando, cantidad de columnas como de filas y orden aleatorio
+    for (let column = 0; column < level.size; column++) {
+        for (let row = 0; row < level.size; row++) {
+            const smallBox = document.createElement('div');
+            let randomEmojis = Math.floor(Math.random() * emojiRose.length);
+            smallBox.innerHTML = emojiRose[randomEmojis];
+            smallBox.className = key;
+        }
+    }
+}
